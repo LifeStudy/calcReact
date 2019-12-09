11474fc87bb20df74d04814937bbfa893f3c0c5d
arjaraujo <bruno96araujo@gmail.com>
2019-12-09 19:05:24 -0300

    init page Calculator

---
+++ src/pages/Calculator.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from '../components'
+
+// import { Container } from './styles';
+
+export default class Calculator extends Component {
+  render() {
+    return(
+        <div>
+            Calculator
+            <Button />
+        </div>
+    );
+  }
+}
