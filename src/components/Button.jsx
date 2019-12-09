d160dc07584785323756d3793aa3f45e16f5e9b5
arjaraujo <bruno96araujo@gmail.com>
2019-12-09 19:05:59 -0300

    init component Button

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Button extends Component {
+  render() {
+    return(
+        <div>
+            Button
+        </div>
+    );
+  }
+}
